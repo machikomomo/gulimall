716148de0d4708676e69bf9ae610854434d60868
machikomomo <3084096374@qq.com>
2023-06-10 23:17:08 +0800

    创建微服务、数据库sql导入、逆向工程、SpringBoot整合Mybatis、brew maven并修改idea maven配置、product服务exclusion common模块lombok

---
+++ gulimall-product/src/main/java/cn/odyssey/gulimall/gulimallproduct/GulimallProductApplication.java
@@ -0,0 +1,15 @@
+package cn.odyssey.gulimall.gulimallproduct;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@MapperScan("cn.odyssey.gulimall.gulimallproduct.dao")
+@SpringBootApplication
+public class GulimallProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallProductApplication.class, args);
+    }
+
+}
